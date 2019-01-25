56ec0a5c752b2f08f6a4e1a9e807d267eacc1df3
Dipen Ved <dive@microsoft.com>
2019-01-25 11:51:35 +0530

    add more routes and travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   #get 'welcome/index'
   # match '(:controller(/:action/(/:id)))', :via => :get
   get 'welcome/awesome' => 'welcome#awesome'
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_index_url
-    assert_response :success
-  end
-
 end
